8f50a28a39b017c7da8c5046627bc50b8fc0a227
HaniefuL <haniefulgeofarry@gmail.com>
2026-03-03 13:27:12 +0700

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas Pertemuan 1</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body class="bg-zinc-950 flex items-center justify-center min-h-screen">
+    <div class="bg-zinc-900 border border-zinc-800 p-10 rounded-lg shadow-xl w-full max-w-md">
+        <h1 class="text-white font-semibold text-lg">Hanieful Geofarry juneo Syahyana</h1> <p class="text-zinc-500 mb-6">20240140018</p> <button class="bg-white text-black px-4 py-2 rounded-md font-medium hover:bg-zinc-200 transition">
+            Modul Pertemuan 1
+        </button>
+    </div>
+</body>
+</html>
