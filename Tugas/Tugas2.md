9bf911cc0d45516d2606b5010a8a19e13f2aa4c7
HaniefuL <haniefulgeofarry@gmail.com>
2026-03-06 21:34:15 +0700

    tugas2

---
+++ Tugas/Tugas2.md
@@ -0,0 +1,6 @@
+Register
+![alt text](image-1.png)
+Login
+![alt text](image-2.png)
+Dashboard
+![alt text](image-3.png)
---
+++ resources/views/about.blade.php
@@ -0,0 +1,22 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
+            {{ __('Biodata Mahasiswa') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="space-y-4">
+                        <p><strong>Nama:</strong> Hanieful Geofarry Juneo Syahyana</p>
+                        <p><strong>NIM:</strong> 20230140074</p>
+                        <p><strong>Prodi:</strong> Teknologi Informasi</p>
+                        <p><strong>Hobi:</strong> Sepak Bola</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
